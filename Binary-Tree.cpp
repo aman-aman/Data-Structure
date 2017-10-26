06bef37c91062e59dd22df75a93b0171580b038f
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-10-26 15:13:18 +0530

    Add files via upload

---
+++ Binary-Tree.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+//aman kumar jha
+struct node
+{
+	int data;
+	node *left;
+	node *right;
+};
+node* insert(int data)
+{
+	node *temp=new node();
+	temp->data=data;
+	temp->left=NULL;
+	temp->right=NULL;
+	return temp;
+}
+int main()
+{
+    node *root=new node();
+    int a[5]={1,2,3,4,5};
+    root=insert(1);
+    root->left=insert(2);
+    root->right=insert(3);
+    root->left->left=insert(4);
+    root->left->right=insert(5);
+
+}
